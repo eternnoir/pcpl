2ea0549d741c583794a72c3e33fcaa1ba7d9f2a7
eternnoir <eternnoir@gmail.com>
2013-04-20 20:06:48 +0800

    add TraceMode

---
+++ pcpl.core/src/pcpl/core/mode/TraceMode.java
@@ -0,0 +1,10 @@
+package pcpl.core.mode;
+
+import pcpl.core.eventHandler.BreakPointListener;
+import pcpl.core.eventHandler.TargetCreationListener;
+import pcpl.core.eventHandler.TargetTerminationListener;
+
+public class TraceMode extends AbstractMode implements BreakPointListener,
+		TargetCreationListener, TargetTerminationListener {
+
+}
