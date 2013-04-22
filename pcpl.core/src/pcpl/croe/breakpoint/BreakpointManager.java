e686126003c14e956e3632308516430f8371f145
FrankWang <eternnoir@gmail.com>
2013-04-22 16:55:54 +0800

    add diff

--- pcpl.core/src/pcpl/croe/breakpoint/BreakpointManager.java
+++ pcpl.core/src/pcpl/croe/breakpoint/BreakpointManager.java
@@ -1,5 +1,39 @@
 package pcpl.croe.breakpoint;
 
+import java.util.ArrayList;
+
+import org.eclipse.core.resources.IMarker;
+
+
+
+
 public class BreakpointManager {
+	private static BreakpointManager instance = null;
+	
+	public static BreakpointManager getInstance() {
+		if (instance == null) {
+			instance = new BreakpointManager();
+		}
+		return instance;
+	}
+	
+	public BreakpointManager(){
+		
+	}
+	
+	public ArrayList<IMarker> diffResult(ArrayList<IMarker> nor,ArrayList<IMarker> rec){
+		ArrayList<IMarker> _ret = new ArrayList<IMarker>();
+		for(IMarker nm : nor){
+			for(IMarker re : rec){
+				if(nm.equals(re)){
+					_ret.add(re);
+				}
+			}
+		}
+		for(IMarker sel : _ret){
+			rec.remove(sel);
+		}
+		return rec;
+	}
 
 }
