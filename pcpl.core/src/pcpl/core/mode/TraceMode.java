a63c69a9c9a213e54ae06cc0b144cddf0021521c
Frenk Wang <marksein@gmail.com>
2013-04-23 15:29:04 +0800

    tracemode

--- pcpl.core/src/pcpl/core/mode/TraceMode.java
+++ pcpl.core/src/pcpl/core/mode/TraceMode.java
@@ -1,10 +1,37 @@
 package pcpl.core.mode;
 
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.debug.core.model.IDebugTarget;
+import org.eclipse.debug.core.model.ILineBreakpoint;
+import org.eclipse.debug.core.model.IVariable;
+
 import pcpl.core.eventHandler.BreakPointListener;
+import pcpl.core.eventHandler.EventCenter;
 import pcpl.core.eventHandler.TargetCreationListener;
 import pcpl.core.eventHandler.TargetTerminationListener;
+import pcpl.croe.breakpoint.BreakpointManager;
 
 public class TraceMode extends AbstractMode implements BreakPointListener,
 		TargetCreationListener, TargetTerminationListener {
+	private IDebugTarget[] _debugTargets = null;
+	public TraceMode(){
+		_modeType = 3;
+		BreakpointManager.getInstance().removeAllBreakpoint();
+		BreakpointManager.getInstance().setResult();
+	}
+	
+	public void onBreakPointTriggered(IVariable[] variables,IBreakpoint breakpoint) {
+		//ILineBreakpoint lineBreakpoint = (ILineBreakpoint) breakpoint;
+		//int mode = EventCenter.getInstance().getModeType();
+		_debugTargets = DebugPlugin.getDefault().getLaunchManager().getDebugTargets();
+		try{
+		}
+		catch(Exception ex){
+			System.err.print("get breakpoint info error");
+		}	
+
+	}
 
 }
