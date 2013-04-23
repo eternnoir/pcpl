5284e5538b977e195fcdf85745d06d58b17684b0
Frenk Wang <marksein@gmail.com>
2013-04-23 15:29:41 +0800

    add RemovePoint & Set Result

--- pcpl.core/src/pcpl/croe/breakpoint/BreakpointManager.java
+++ pcpl.core/src/pcpl/croe/breakpoint/BreakpointManager.java
@@ -3,15 +3,16 @@ package pcpl.croe.breakpoint;
 import java.util.ArrayList;
 
 import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.model.Breakpoint;
+import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.core.model.ILineBreakpoint;
 import org.eclipse.debug.core.model.LineBreakpoint;
 
-
-
-
 public class BreakpointManager {
 	private static BreakpointManager instance = null;
-	
+	private  ArrayList<ILineBreakpoint> _result = null;
 	public static BreakpointManager getInstance() {
 		if (instance == null) {
 			instance = new BreakpointManager();
@@ -20,19 +21,48 @@ public class BreakpointManager {
 	}
 	
 	public BreakpointManager(){
-		
+		_result = null;
 	}
 	
 	public ArrayList<ILineBreakpoint> diffResult(ArrayList<ILineBreakpoint> nor,ArrayList<ILineBreakpoint> rec){
-		ArrayList<ILineBreakpoint> _ret = new ArrayList<ILineBreakpoint>();
-		int i;
 		for(ILineBreakpoint nm : nor){
 			while(rec.indexOf(nm)!= -1){
 				rec.remove(nm);
 			}
-
 		}
-		return rec;
+		_result = new ArrayList<ILineBreakpoint>(rec.size());
+		for(ILineBreakpoint b : rec){
+			_result.add(b);
+		}
+		return _result;
+	}
+	
+	public ArrayList<ILineBreakpoint> getResult(){
+		assert(_result != null);
+		return _result;
+	}
+	
+	public void removeAllBreakpoint(){
+		IBreakpoint[] b = DebugPlugin.getDefault().getBreakpointManager().getBreakpoints();
+		for(IBreakpoint _b : b){
+			try {
+				_b.setEnabled(false);
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void setResult(){
+		for(ILineBreakpoint b : _result){
+			try {
+				b.setEnabled(true);
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
