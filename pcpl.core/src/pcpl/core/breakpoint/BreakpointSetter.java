4ab9edd8a115ebcd1c26a824a766924e3a15ce0e
eternnoir <eternnoir@gmail.com>
2013-04-27 15:57:22 +0800

    srt_all

--- pcpl.core/src/pcpl/core/breakpoint/BreakpointSetter.java
+++ pcpl.core/src/pcpl/core/breakpoint/BreakpointSetter.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.core.model.ILineBreakpoint;
@@ -30,9 +33,11 @@ public class BreakpointSetter {
 	}
 	
 	public void setBreakpoint(IResource resource,int lineNum){
+		String typeName = FileParaviserUtils.getClassName(resource);	//javabreakpoint need this
 		try {
-			IBreakpoint bp = JDIDebugModel.createLineBreakpoint(resource,"MinervaVII", 12, -1, -1, 0,true,null);
+			IBreakpoint bp = JDIDebugModel.createLineBreakpoint(resource,typeName, lineNum, -1, -1, 0,true,null);
 			DebugPlugin.getDefault().getBreakpointManager().addBreakpoint(bp);
+			System.out.print("setBreakpointat "+typeName+":"+lineNum+"\n");
 		} catch (CoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
