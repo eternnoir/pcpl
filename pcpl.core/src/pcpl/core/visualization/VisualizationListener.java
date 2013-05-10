7c6d1ad087db589ac9a1af0b2886265037d2b965
eternnoir <eternnoir@gmail.com>
2013-05-11 07:40:43 +0800

    remove some file & add a visualizer

--- pcpl.core/src/pcpl/core/visualization/VisualizationListener.java
+++
@@ -1,9 +0,0 @@
-package pcpl.core.visualization;
-
-import org.eclipse.debug.core.model.IBreakpoint;
-import org.eclipse.debug.core.model.IVariable;
-
-public interface VisualizationListener {
-	public abstract void onBreakPointTriggered(IVariable[] variables,
-			IBreakpoint breakpoint);
-}
