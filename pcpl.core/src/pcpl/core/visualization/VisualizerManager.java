c6b32b4fa2b55ff6ebfeb681466c28b0968876ed
eternnoir <eternnoir@gmail.com>
2013-05-10 14:00:54 +0800

    add Listener&Manager

---
+++ pcpl.core/src/pcpl/core/visualization/VisualizerManager.java
@@ -0,0 +1,15 @@
+package pcpl.core.visualization;
+
+public class VisualizerManager {
+	private static VisualizerManager instance = null;
+	public static VisualizerManager getInstance() {
+		if (instance == null) {
+			instance = new VisualizerManager();
+		}
+		return instance;
+	}
+	
+	public VisualizerManager(){
+	}
+
+}
