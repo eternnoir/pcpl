d3ed952ef12d25a79f75265c72c226225fa3ad20
FrankWang <eternnoir@gmail.com>
2013-05-20 15:21:12 +0800

    remojve biewer menu

--- pcpl.simpleVisualizer/src/pcpl/simplevisualizer/views/BasicView.java
+++ pcpl.simpleVisualizer/src/pcpl/simplevisualizer/views/BasicView.java
@@ -1,5 +1,9 @@
 package pcpl.simplevisualizer.views;
 
+import java.awt.Frame;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.swt.widgets.*;
@@ -8,6 +12,8 @@ public class BasicView extends ViewPart {
 	private Label label;
 	public BasicView(){
 		super();
+		Composite composite = new Composite( null, SWT.EMBEDDED | SWT.NO_BACKGROUND);
+		Frame frame = SWT_AWT.new_Frame(composite);
 	}
 	@Override
 	public void createPartControl(Composite parent) {
