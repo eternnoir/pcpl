28c53f50ebd67175217a9818275ed9816e856336
eternnoir <eternnoir@gmail.com>
2013-06-10 14:31:59 +0800

    n

--- pcpl.core/src/pcpl/core/StartUp.java
+++ pcpl.core/src/pcpl/core/StartUp.java
@@ -3,11 +3,7 @@ package pcpl.core;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.ui.IStartup;
 
-import pcpl.core.eventHandler.EventCenter;
 import pcpl.core.launch.pcplLauncher;
-import pcpl.core.mode.AbstractMode;
-import pcpl.core.mode.NormalMode;
-import pcpl.core.mode.RecordMode;
 
 public class StartUp implements IStartup {
 
