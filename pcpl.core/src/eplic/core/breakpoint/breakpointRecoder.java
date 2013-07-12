c8a5407c4ae729a34610de70e5ae5e274a684e0f
FrankWang <eternnoir@gmail.com>
2013-07-12 15:40:55 +0800

    fix warning

--- pcpl.core/src/eplic/core/breakpoint/breakpointRecoder.java
+++ pcpl.core/src/eplic/core/breakpoint/breakpointRecoder.java
@@ -2,13 +2,14 @@ package eplic.core.breakpoint;
 
 import java.util.ArrayList;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.model.ILineBreakpoint;
 /**
  * EPLIC - A Tool to Assist Locating Interested Code.
  * Copyright (C) 2013 Frank Wang <eternnoir@gmail.com>
  * 
  * This file is part of EPLIC.
+ * 
+ * 
  * 
  * @author FrankWang
  *
